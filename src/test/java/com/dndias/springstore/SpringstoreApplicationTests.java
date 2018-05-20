d3a572e587dc37bdf4e8aa698e93fc470e33f857
Daniel Dias <danielnunes@hotmail.com>
2018-05-19 23:59:57 -0400

    Initial Version. Project Created

---
+++ src/test/java/com/dndias/springstore/SpringstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dndias.springstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringstoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
