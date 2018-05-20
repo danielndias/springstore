5e06e3bb9f614b373f23e536fa6925c431273db5
Daniel Dias <danielnunes@hotmail.com>
2018-05-20 00:43:02 -0400

    Testing the first domain class - Category

--- src/main/java/com/dndias/springstore/resources/CategoryResource.java
+++ src/main/java/com/dndias/springstore/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.dndias.springstore.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import 	org.springframework.web.bind.annotation.RestController;
+
+import com.dndias.springstore.domain.Category;
 
 @RestController
 @RequestMapping(value="/category")
 public class CategoryResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "REST is working!!!";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Computers");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
