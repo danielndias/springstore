a45fe07dc9bf7d3cfa2c5bd30c657b1774a5b693
Daniel Dias <danielnunes@hotmail.com>
2018-07-11 09:45:50 -0400

    H2 Database and db auto creation

--- src/main/java/com/dndias/springstore/domain/Category.java
+++ src/main/java/com/dndias/springstore/domain/Category.java
@@ -2,15 +2,23 @@ package com.dndias.springstore.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Class to model the Product's Categories
  * 
  * @author daniel
  *
  */
+@Entity
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	
